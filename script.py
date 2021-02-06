92877ab9e6bfd201d17a8733879c6951bbb9cd7d
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-06 13:09:22 -0600

    added docstring

--- script.py
+++ script.py
@@ -4,6 +4,14 @@ import re
 
 ###--- FUNCTIONS ---###
 def check_strenght():
+    '''
+      password must have: 
+      - 8 or more characters, less than or exactly 20.
+      - at least 1 digit.
+      - at least 1 lowercase character
+      - at least 1 uppercase character
+      - at least 1 special character
+    '''
     user_input = str(input("Enter the password:\n"))
 
     if(len(user_input) >= 8):
